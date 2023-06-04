a30a58f64d438125b66abc24fac574f7dae967fd
Rercks <127117691+Rercks@users.noreply.github.com>
2023-06-03 20:02:56 -0600

    Add files via upload

---
+++ jmain.js
@@ -0,0 +1,55 @@
+// array of images
+const images = [
+    'imgjardin/d.jpeg', 
+    'imgjardin/e.jpeg', 
+    'imgjardin/f.jpeg',
+];
+
+const firstImage = 0;
+const lastImage = images.length -1;
+let currentImage = 0;
+
+//next
+const nextBtn = document.getElementById('next');
+nextBtn.addEventListener('click',()=>{
+
+        // get image tag
+        const imageTag = document.getElementById('image');
+        currentImage++;
+        if(currentImage >= lastImage){
+            currentImage= 2;
+        }
+        imageTag.src = images[currentImage];
+        document.getElementById('info').innerHTML = (currentImage +1) + '/3';
+});
+
+//prev
+const preBtn = document.getElementById('prev');
+preBtn.addEventListener('click',()=>{
+
+    // get image tag
+    const imageTag = document.getElementById('image');
+    currentImage--;
+    if(currentImage <= firstImage){
+        currentImage= 0;
+    }
+    imageTag.src = images[currentImage];
+    document.getElementById('info').innerHTML = (currentImage +1) + '/3';
+
+});
+
+let toggle_bar = document.querySelector(".nav-header");
+
+let sidebar = document.querySelector(".sidebar");
+
+toggle_bar.addEventListener("click", function(){
+    if(toggle_bar.firstElementChild.classList.contains("fa-bars"))
+    {
+        toggle_bar.firstElementChild.classList.replace("fa-bars","fa-times");
+    }
+    else
+    {
+        toggle_bar.firstElementChild.classList.replace("fa-times","fa-bars");
+    }
+   sidebar.classList.toggle("sidebaractive")
+});
